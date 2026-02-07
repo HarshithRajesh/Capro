cc31584a194d0bd2363b475a630dd3ed52dd10af
harshith <harshithr2004@gmail.com>
2026-02-07 18:06:05 +0530

    Caching logic added

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,14 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
-
-var clearCache bool
 
 type Cache struct {
 	Status int               `json:"status"`
@@ -17,9 +19,72 @@ type Cache struct {
 	Header map[string]string `json:"headers"`
 }
 
-var cache = make(map[string]Cache)
+var (
+	err        error
+	cache      = make(map[string]Cache)
+	port       int
+	origin     string
+	clearCache bool
+)
 
 const cacheFile = "cache.json"
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+
+	if entry, found := cache[path]; found {
+		fmt.Printf("Cache-Hit :%s\n", path)
+		w.Header().Set("X-Cache", "HIT")
+		for k, v := range entry.Header {
+			w.Header().Set(k, v)
+		}
+		w.WriteHeader(entry.Status)
+
+		if _, err = w.Write(entry.Body); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		return
+	}
+
+	fmt.Printf("Cache-MISS :%s\n", path)
+	newPath := strings.TrimSuffix(origin, "/") + path
+	req, err := http.Get(newPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer req.Body.Close()
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	respCode := req.StatusCode
+	if respCode != 200 {
+		log.Fatal("invalid url")
+		os.Exit(1)
+	}
+
+	originalHeaders := req.Header
+	headerMap := make(map[string]string)
+	for k := range originalHeaders {
+		headerMap[k] = req.Header.Get(k)
+	}
+	bodyString := string(body)
+
+	entry := Cache{
+		Status: respCode,
+		Body:   []byte(bodyString),
+		Header: headerMap,
+	}
+
+	updateCache(path, entry)
+
+	w.Header().Set("X-Cache", "MISS")
+	w.Header().Set("Content-type", entry.Header["Content-type"])
+	w.WriteHeader(entry.Status)
+	w.Write([]byte(bodyString))
+}
 
 func getCache() {
 	file, err := os.ReadFile(cacheFile)
@@ -60,12 +125,12 @@ var rootCmd = &cobra.Command{
 			return
 		}
 
-		port, err := cmd.Flags().GetInt("port")
+		port, err = cmd.Flags().GetInt("port")
 		if err != nil {
 			fmt.Printf("Using default port 8080\n")
 			port = 8080
 		}
-		origin, err := cmd.Flags().GetString("origin")
+		origin, err = cmd.Flags().GetString("origin")
 		if err != nil {
 			fmt.Print("No origin was specified")
 		}
@@ -74,7 +139,14 @@ var rootCmd = &cobra.Command{
 			return
 		}
 		getCache()
-
+		http.HandleFunc("/", proxyHandler)
+		fmt.Printf("Starting Proxy Server on port %d\n", port)
+		fmt.Printf("Forwarding requests to: %s\n", origin)
+		portadr := fmt.Sprintf(":%d", port)
+		err = http.ListenAndServe(portadr, nil)
+		if err != nil {
+			fmt.Printf("Failed to start server: %v\n", err)
+		}
 		fmt.Printf("Starting server on %d at %s\n", port, origin)
 	},
 }
